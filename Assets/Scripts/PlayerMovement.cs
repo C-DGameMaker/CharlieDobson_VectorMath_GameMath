e5572dbdfb8e74de229f6fb1944690a04bcb7d00
C-DGameMaker <charlotte5634@outlook.com>
2025-11-03 13:51:26 -0400

    Updated PlayMenu and Added Audio Source

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public GameObject childOfCube;
+    private Vector3 startPos;
+    static int movement = 0;
+
+    public GameObject enemyObject;
+    private Vector3 enemyDir;
+
+    public float playerDotEnemyDir;
+    void Start()
+    {
+        startPos = transform.position;
+        childOfCube.transform.position = startPos + transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            movement++;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            movement--;
+        }
+
+        transform.position = startPos + new Vector3(movement, 0, movement);
+
+        enemyDir = enemyObject.transform.position;
+
+        playerDotEnemyDir = Vector3.Dot(transform.forward, enemyDir);
+    }
+}
